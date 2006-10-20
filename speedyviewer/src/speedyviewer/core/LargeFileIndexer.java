8fcbf655c54962a22a2109204b40d78cfad6bcbe
fabrizio.iannetti <fabrizio.iannetti@628da645-981f-0410-b7fc-e9261d6723c2>
2006-10-20 07:47:31 +0000

    added thread for disk reading

--- speedyviewer/src/speedyviewer/core/LargeFileIndexer.java
+++ speedyviewer/src/speedyviewer/core/LargeFileIndexer.java
@@ -5,12 +5,23 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.SynchronousQueue;
 
 public class LargeFileIndexer
 {
 	int chunkSize;
+	int count = 0;
+
+	private Exchanger<DataBuffer> exchanger = new Exchanger<DataBuffer>();
 	private ArrayList<int[]> indexCache = new ArrayList<int[]>();
 	
+	class DataBuffer
+	{
+		byte[] buffer = new byte[1024*256];
+		int used = 0;
+	}
+
 	/**
 	 * Constructor.
 	 * 
@@ -30,9 +41,9 @@ public class LargeFileIndexer
 		int offset = 0;
 		int line = 0;
 		int len;
-
+	
 		FileInputStream inputStream = new FileInputStream(file);
-
+	
 		while( (len = inputStream.read(buffer)) > 0)
 		{
 			for(int i=0 ; i < len ; i++)
@@ -45,7 +56,7 @@ public class LargeFileIndexer
 					//chunk filled?
 					if(line == indexChunk.length)
 					{
-						sendIndexChunk(indexChunk);
+						sendIndexChunk(indexChunk, line);
 						indexChunk = new int[chunkSize];
 						line = 0;
 					}
@@ -55,11 +66,129 @@ public class LargeFileIndexer
 			offset += len;
 		}
 		
+		//last chunk (if not completely filled)
+		if(line > 0)
+			sendIndexChunk(indexChunk, line);
+	
 		inputStream.close();
 	}
 	
-	private void sendIndexChunk(int[] indexChunk)
+	private void sendIndexChunk(int[] indexChunk, int len)
 	{
 		indexCache.add(indexChunk);
+		count += len;
 	}
-}
+	
+	public int getLineCount()
+	{
+		return count;
+	}
+
+	public int getOffsetForLine(int line)
+	{
+		int chunk;
+		int offset;
+		
+		chunk = line / chunkSize;
+		offset = line % chunkSize;
+		
+		return indexCache.get(chunk)[offset];
+	}
+
+
+	public void parallelIndex(File file) throws IOException
+	{
+		int[] indexChunk = new int[chunkSize];
+		int offset = 0;
+		int line = 0;
+		int len;
+		DataBuffer buffer = new DataBuffer();
+		
+		FillingLoop filler = new FillingLoop(file);
+		new Thread(filler).start();
+
+		try
+		{
+			while( (buffer = exchanger.exchange(buffer)) != null )
+			{
+				len = buffer.used;
+
+				for(int i=0 ; i < len ; i++)
+				{
+					//check for newline: ascii code 10
+					if(buffer.buffer[i] == 10)
+					{
+						indexChunk[line++] = offset + i + 1;
+						
+						//chunk filled?
+						if(line == indexChunk.length)
+						{
+							sendIndexChunk(indexChunk, line);
+							indexChunk = new int[chunkSize];
+							line = 0;
+						}
+					}
+				}
+				//going to read next buffer, update offset
+				offset += len;
+			}
+		} catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//last chunk (if not completely filled)
+		if(line > 0)
+			sendIndexChunk(indexChunk, line);
+	
+	}
+
+	class FillingLoop implements Runnable
+	{
+		private File file;
+		
+		public FillingLoop(File file)
+		{
+			super();
+			this.file = file;
+		}
+
+		public void run()
+		{
+			int len;
+			DataBuffer buffer = new DataBuffer();
+			FileInputStream inputStream = null;
+
+			try
+			{
+				inputStream = new FileInputStream(file);
+
+				while ((len = inputStream.read(buffer.buffer)) > 0)
+				{
+					buffer.used = len;
+					//exchange the buffers
+					buffer = exchanger.exchange(buffer);
+				}
+				
+				exchanger.exchange(null);
+
+				inputStream.close();
+
+			} catch (InterruptedException ex)
+			{
+				// TODO
+			} catch (FileNotFoundException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	}
