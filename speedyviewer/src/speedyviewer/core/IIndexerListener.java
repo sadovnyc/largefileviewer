c82df5d6840aa0eb4c758db5580ed79a76b9afb6
fabrizio.iannetti <fabrizio.iannetti@628da645-981f-0410-b7fc-e9261d6723c2>
2006-10-23 12:03:57 +0000

    index in a separate thread

---
+++ speedyviewer/src/speedyviewer/core/IIndexerListener.java
@@ -0,0 +1,16 @@
+package speedyviewer.core;
+
+/**
+ * Listener to events from the indexer, currently only
+ * when a new index chunk is available.
+ * 
+ */
+public interface IIndexerListener
+{
+	/**
+	 * A new chunk of line offsets is available in the indexer.
+	 * 
+	 * @param indexer the indexer
+	 */
+	public void newIndexChunk(LargeFileIndexer indexer);
+}
--- speedyviewer/src/speedyviewer/core/IndexerThread.java
+++ speedyviewer/src/speedyviewer/core/IndexerThread.java
@@ -1,24 +1,41 @@
 package speedyviewer.core;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- * @author  fiannetti
+ * Thread that uses a FileIndexer to build a
+ * map of line numbers to offests for a given file.
+ * 
+ * Calling start() on this class will start the indexing,
+ * progress is reported via the indexer listener.
  */
 public class IndexerThread extends Thread
 {
+	//the file to index
 	private File file;
+
+	//the indexer
 	private LargeFileIndexer indexer;
 	
-	public IndexerThread(int chunkSize, File file) throws FileNotFoundException
+	/**
+	 * Creates a thread to index the given file.
+	 * 
+	 * @param chunkSize the size of index chunks.
+	 * @param file the file to index.
+	 */
+	public IndexerThread(int chunkSize, File file)
 	{
 		super("indexing:" + file.getPath());
 		this.file = file;
 		this.indexer = new LargeFileIndexer(chunkSize);
 	}
 
+	/**
+	 * Implementation of run.
+	 *  
+	 * @see java.lang.Thread#run()
+	 */
 	public void run()
 	{
 		try {
@@ -31,11 +48,32 @@ public class IndexerThread extends Thread
 
 	public synchronized int getLineCount()
 	{
-		return indexer.chunkSize;
+		return indexer.getLineCount();
 	}
 	
-	public synchronized int[] getLineDelimiters(int index)
+	/**
+	 * Get the offsets of beginning (first character)
+	 * and end (line separator) of a line.
+	 * Thus, the line spans from values[0] to
+	 * values[1] in the file.
+	 * 
+	 * @param line the line number 
+	 * @param values result array (must have length >= 2)
+	 */
+	public synchronized void getLineDelimiters(int line, int[] values)
 	{
-		return null;
+		values[0] = indexer.getOffsetForLine(line);
+		values[1] = indexer.getOffsetForLine(line+1);
+	}
+	
+	/**
+	 * Set the index listener.
+	 * The listener is called from the indexer thread.
+	 * 
+	 * @param listener
+	 */
+	public void setListener(IIndexerListener listener)
+	{
+		indexer.setListener(listener);
 	}
 }
