914493a7f460a0b4dc87912f12e1e1c205e90a79
fabrizio.iannetti <fabrizio.iannetti@628da645-981f-0410-b7fc-e9261d6723c2>
2006-10-23 12:03:57 +0000

    index in a separate thread

---
+++ trunk/speedyviewer/src/speedyviewer/core/DualThreadIndexer.java
@@ -0,0 +1,137 @@
+package speedyviewer.core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.Exchanger;
+
+public class DualThreadIndexer extends LargeFileIndexer
+{
+	private Exchanger<DataBuffer> exchanger = new Exchanger<DataBuffer>();
+	private class DataBuffer
+	{
+		byte[] buffer = new byte[1024*256];
+		int used = 0;
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param file
+	 * @param chunkSize
+	 * @throws FileNotFoundException
+	 */
+	public DualThreadIndexer(int chunkSize) throws FileNotFoundException
+	{
+		super(chunkSize);
+	}
+
+	public void index(File file) throws IOException
+	{
+		int[] indexChunk = new int[chunkSize];
+		int offset = 0;
+		int line = 0;
+		int len;
+		DataBuffer buffer = new DataBuffer();
+		long start = 0;
+		long computation = 0;
+		long reading = 0;
+		
+		start = System.currentTimeMillis();
+
+		FillingLoop filler = new FillingLoop(file);
+		new Thread(filler).start();
+
+		try
+		{
+			while( (buffer = exchanger.exchange(buffer)) != null )
+			{
+				len = buffer.used;
+
+				reading += System.currentTimeMillis() - start;
+				start = System.currentTimeMillis();
+
+				for(int i=0 ; i < len ; i++)
+				{
+					//check for newline: ascii code 10
+					if(buffer.buffer[i] == 10)
+					{
+						indexChunk[line++] = offset + i + 1;
+						
+						//chunk filled?
+						if(line == indexChunk.length)
+						{
+							sendIndexChunk(indexChunk, line);
+							indexChunk = new int[chunkSize];
+							line = 0;
+						}
+					}
+				}
+				//going to read next buffer, update offset
+				offset += len;
+				computation += System.currentTimeMillis() - start;
+				start = System.currentTimeMillis();
+			}
+		} catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//last chunk (if not completely filled)
+		if(line > 0)
+			sendIndexChunk(indexChunk, line);
+
+		System.out.println("computation time (ms):" + computation + " reading time (ms):" + reading);
+	}
+
+	
+	private class FillingLoop implements Runnable
+	{
+		private File file;
+		
+		public FillingLoop(File file)
+		{
+			super();
+			this.file = file;
+		}
+
+		public void run()
+		{
+			int len;
+			DataBuffer buffer = new DataBuffer();
+			FileInputStream inputStream = null;
+
+			try
+			{
+				inputStream = new FileInputStream(file);
+
+				while ((len = inputStream.read(buffer.buffer)) > 0)
+				{
+					buffer.used = len;
+					//exchange the buffers
+					buffer = exchanger.exchange(buffer);
+				}
+				
+				exchanger.exchange(null);
+
+				inputStream.close();
+
+			} catch (InterruptedException ex)
+			{
+				// TODO
+			} catch (FileNotFoundException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	}
--- trunk/speedyviewer/src/speedyviewer/views/SpeedyView.java
+++ trunk/speedyviewer/src/speedyviewer/views/SpeedyView.java
@@ -1,79 +1,81 @@
 package speedyviewer.views;
 
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.*;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.SWT;
+import org.eclipse.ui.part.ViewPart;
 
 import speedyviewer.core.LargeFileIndexer;
+import speedyviewer.core.IIndexerListener;
+import speedyviewer.core.IndexerThread;
 
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * This view shows a large file.
  */
-
-public class SpeedyView extends ViewPart {
+public class SpeedyView extends ViewPart
+{
 	private StyledText viewer;
 
+	/**
+	 * Update the view
+	 *
+	 */
+	private class ViewUpdate implements Runnable
+	{
+		//new line count
+		private int count;
+
+		public ViewUpdate(int count)
+		{
+			this.count = count;
+		}
+
+		public void run()
+		{
+			//just show the new length
+			setContentDescription("lines:" + count);
+		}
+	};
+
+	private IIndexerListener listener = new IIndexerListener() {
+		public void newIndexChunk(LargeFileIndexer indexer)
+		{
+			//run update in the UI thread (this listener is called
+			//from the indexer thread and can not call UI methods)
+			SpeedyView.this.getViewSite().getShell().getDisplay().asyncExec(new ViewUpdate(indexer.getLineCount()));
+		}
+	};
 
 	/**
 	 * The constructor.
 	 */
-	public SpeedyView() {
+	public SpeedyView()
+	{
+		super();
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * Create the view widgetry.
+	 * 
+	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
 	 */
 	public void createPartControl(Composite parent)
 	{
 		viewer = new StyledText(parent, SWT.NONE);
-		try {
-			System.out.println("Starting");
-			long start = System.currentTimeMillis();
-			LargeFileIndexer indexer = new LargeFileIndexer(64*1024);
-			indexer.index(new File("C:\\ccviews\\fia060925_avm.prv\\result\\vc7\\Lgreg_high_optiverse_tri_band_all_gcc.txt"));
-			System.out.println("Elapsed Time (ms): " + (System.currentTimeMillis() - start));
-			System.out.println("Lines counted: " + indexer.getLineCount());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//create and start indexer thread here
+		IndexerThread indexer = new IndexerThread(64*1024,new File("/home/fab/ciccione.txt"));
+		indexer.setListener(listener);
+		indexer.start();
 	}
-
-
-
-
-	
-
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
-	public void setFocus() {
+	public void setFocus()
+	{
 		viewer.setFocus();
 	}
 }
